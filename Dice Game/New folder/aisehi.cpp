0842de8b8d56a74b0967a1b854eff17cba0a6fba
[Priyank Kulshrestha] <[priyank26june@gmail.com]>
2020-04-05 05:52:43 +0530

    dice game --done

---
+++ Dice Game/New folder/aisehi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+          cout<<endl;
+          int arr[5];
+          for (int  i = 0; i < 5; i++)
+          {
+                    /* code */
+                    cin>>arr[i];
+          }
+          for (int i = 0; i < 5; i++)
+          {
+                    /* code */
+                    cout << arr[i] << endl;
+          }
+          
+           return 0;
+}
